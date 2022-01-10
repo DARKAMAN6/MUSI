19ad1193b3c693285b91164f29ef9df490be02eb
DARKAMAN6 <89340128+DARKAMAN6@users.noreply.github.com>
2022-01-10 17:39:42 +0530

    Create venom.py

---
+++ KennedyMusic/handlers/venom.py
@@ -0,0 +1,17 @@
+@Client.on_message(
+    filters.user(SUDO_USERS) & filters.command(["restart"], prefixes=f"{HNDLR}")
+)
+async def restart(client, m: Message):
+    await m.delete()
+    loli = await m.reply("𝟷")
+    await loli.edit("𝟸")
+    await loli.edit("𝟹")
+    await loli.edit("𝟺")
+    await loli.edit("𝟻")
+    await loli.edit("𝟼")
+    await loli.edit("𝟽")
+    await loli.edit("𝟾")
+    await loli.edit("𝟿")
+    await loli.edit("**✨𝙼𝚄𝚂𝙸𝙲 𝙱𝙾𝚃 𝚁𝙴𝚂𝚃𝙰𝚁𝚃𝙴𝙳👻**")
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    quit()
