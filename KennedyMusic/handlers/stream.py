ce86ad9c19f82cf50234869eaa14639bfded6774
DARKAMAN6 <89340128+DARKAMAN6@users.noreply.github.com>
2022-01-10 11:27:09 +0530

    Update stream.py

--- KennedyMusic/handlers/stream.py
+++ KennedyMusic/handlers/stream.py
@@ -5,7 +5,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 from KennedyMusic.callsmusic import callsmusic, queues
 
-from KennedyMusic.converter.converter import converter
+import KennedyMusic.converter
 from KennedyMusic.downloaders import youtube
 
 from KennedyMusic.config import (
