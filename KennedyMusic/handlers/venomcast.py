f98c3a5031ca872516de4222bd7bf4334d6bbfad
DARKAMAN6 <89340128+DARKAMAN6@users.noreply.github.com>
2022-01-10 15:52:26 +0530

    Create venomcast.py

---
+++ KennedyMusic/handlers/venomcast.py
@@ -0,0 +1,34 @@
+# Originally written by levina on github
+# Broadcast function
+
+
+import asyncio
+
+from pyrogram import Client, filters
+from pyrogram.types import Dialog, Chat, Message
+from pyrogram.errors import UserAlreadyParticipant
+
+from KennedyMusic.callsmusic.callsmusic import client as Bot
+from KennedyMusic.config import SUDO_USERS
+
+@Client.on_message(filters.command(["dcast"]))
+async def dcast(_, message: Message):
+    sent=0
+    failed=0
+    if message.from_user.id not in SUDO_USERS:
+        return
+    else:
+        wtf = await message.reply("`𝚂𝚃𝙰𝚁𝚃𝙸𝙽𝙶 𝚅𝙲𝙰𝚂𝚃🔥`")
+        if not message.reply_to_message:
+            await wtf.edit("𝙿𝙻𝙴𝙰𝚂𝙴 𝚁𝙴𝙿𝙻𝚈 𝚃𝙾 𝙰 𝙼𝙴𝚂𝚂𝙰𝙶𝙴 𝚃𝙾 𝚂𝚃𝙰𝚁𝚃 𝚅𝙲𝙰𝚂𝚃!")
+            return
+        lmao = message.reply_to_message.text
+        async for dialog in Bot.iter_dialogs():
+            try:
+                await Bot.send_message(dialog.chat.id, lmao)
+                sent = sent+1
+                await wtf.edit(f"`𝚅𝙴𝙽𝙾𝙼𝚄𝚂🔥` \n\n**𝚂𝙴𝙽𝚃 𝚃𝙾 ✘** `{sent}` 𝙲𝙷𝙰𝚃𝚂 \n**𝙵𝙰𝙸𝙻𝙴𝙳 𝙸𝙽 ✘** {failed} 𝙲𝙷𝙰𝚃𝚂")
+                await asyncio.sleep(3)
+            except:
+                failed=failed+1
+        await message.reply_text(f"`𝚅𝙲𝙰𝚂𝚃 𝚂𝚄𝙲𝙲𝙴𝚂𝙵𝚄𝙻𝙻𝚈 🔥` \n\n**sent to:** `{sent}` chats \n**failed in:** {failed} chats")
